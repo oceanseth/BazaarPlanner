d39194b816f7d9517d990c3e44a4f6dc66960652
oceanseth <seth@snapchallenge.com>
2024-12-18 14:21:00 -0800

    fixed img icons on monsters

--- js/Item.js
+++ js/Item.js
@@ -1,9 +1,9 @@
 class Item {
-    constructor({ name, tags, cooldown, enchants, text, bottomtext }) {
-        Object.assign(this, { name, cooldown, enchants, text, bottomtext, tags });
+    constructor({ name, tags, cooldown, enchants, text, bottomtext, icon }) {
+        Object.assign(this, { name, cooldown, enchants, text, bottomtext, tags, icon });
         this.size = tags.includes('Small') ? 1 : tags.includes('Medium') ? 2 : 3;
     }
-    getFromName(itemName) {
+    static getFromName(itemName) {
         return new Item(items[itemName]);
     }
 }
