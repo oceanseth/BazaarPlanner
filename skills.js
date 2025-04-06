59f4417376df2331027871ef952e607a413eaf3a
oceanseth <seth@snapchallenge.com>
2025-04-05 19:38:15 -0700

    fix rarity editing

--- skills.js
+++ skills.js
@@ -1564,7 +1564,7 @@ export const skills = {
   },
   "Hardly Workin'": {
     "text": [
-      "The first (5/10/15) times times you slow each fight, Charge 1 Regeneration item 1 second(s)."
+      "The first (5/10/15) times you slow each fight, Charge 1 Regeneration item 1 second(s)."
     ],
     "tier": 1,
     "tags": [
