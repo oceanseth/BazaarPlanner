377175bad425b262b66051fb334bbcadc5379140
oceanseth <seth@snapchallenge.com>
2025-05-05 11:39:20 -0700

    fix

--- js/BazaarPatcher.js
+++ js/BazaarPatcher.js
@@ -2,7 +2,7 @@ import { items } from '../items.js';
 export class BazaarPatcher {
     static apply() {
         if(items["Pistol Sword"].text[1].match(/^When you use an Ammo item, deal .* damage\.$/i)) {
-            items["Pistol Sword"].text[1] = "When you use an Ammo item, deal damage equal to 1 times this item's damage.";
+            items["Pistol Sword"].text[1] = "When you use an Ammo item, deal damage.";
         }
     }
 }
