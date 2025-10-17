07c2426ccab57568eccc864127520f67005f71de
oceanseth <seth@snapchallenge.com>
2025-10-17 03:48:05 -0700

    fix forklift

--- js/TextMatcher.js
+++ js/TextMatcher.js
@@ -1281,7 +1281,7 @@ TextMatcher.matchers.push({
 });
 //If you have another Tool, Apparel, Tech, Weapon, or Friend, this has (+50/+100/+150) Damage for each. from Forklift
 TextMatcher.matchers.push({
-            regex: /^If you have another (\w+(?:, (?:or )?\w+)*)(?: item)?,? this has (\([^)]+\)|\+?\d+) (\w+)(?: for each)?\.?$/i,
+            regex: /^If you have another (\w+(?:,? (?:or )?\w+)*)(?: item)?,? this has (\([^)]+\)|\+?\d+) (\w+)(?: for each)?\.?$/i,
     func: (item, match)=>{
         const tags = match[1].split(", ");
         const amount = getRarityValue(match[2], item.rarity);
